45219cd5f95974d85e8d0555e0422832761a5408
“Nidhu23” <nidhunanik@gmail.com>
2020-06-11 20:40:53 +0530

    Refactored: Used Methods To Compute And Display Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -6,35 +6,49 @@ public class EmpWage {
 	private static final int partTime=2;
 	private static final int workingDays=20;
 	private static final int maxHrs=100;
-	public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation");
-	Random rand=new Random();
-	int empHr=0,totalHrs=0,totalDays=0,dailyWage=0,diff=0,sum=0;
-	ArrayList<Integer> salary=new ArrayList<>();
-	ArrayList<Integer> totalSalary=new ArrayList<>();
-	while(totalDays<=workingDays && totalHrs<=maxHrs) {
-		totalDays++;
-		int empCheck=rand.nextInt(3);
-		switch(empCheck) {
-			case fullTime: empHr=8;
-							break;
-			case partTime: empHr=4;
-							break;
-			default: empHr=0;
-		}
-		totalHrs+=empHr;
-		if(totalHrs>100) {
-			diff=totalHrs-100;
-			empHr-=diff;
-		}
+	private static int totalDays=0;
+	private static ArrayList<Integer> salary=new ArrayList<>();
+	private static ArrayList<Integer> totalSalary=new ArrayList<>();
+	public EmpWage(ArrayList salary,ArrayList totalSary,int totalDays) {
+		this.salary=salary;
+		this.totalSalary=totalSalary;
+		this.totalDays=totalDays;
+	}
+	public static EmpWage salary() {
+		int empHr=0,totalHrs=0,dailyWage=0,diff=0,sum=0;
+		Random rand=new Random();
+		while(totalDays<=workingDays && totalHrs<=maxHrs) {
+			totalDays++;
+			int empCheck=rand.nextInt(3);
+			switch(empCheck) {
+				case fullTime: empHr=8;
+								break;
+				case partTime: empHr=4;
+								break;
+				default: empHr=0;
+			}
+			totalHrs+=empHr;
+			if(totalHrs>100) {
+				diff=totalHrs-100;
+				empHr-=diff;
+			}
 		dailyWage=ratePerHour*empHr;
 		salary.add(dailyWage);
 		sum+=dailyWage;
 		totalSalary.add(sum);
-	}
-	System.out.println("Day\tDaily Wage\tTotal Salary");
-	for(int i=1;i<totalDays;i++) {
-		System.out.println(i+"\t"+salary.get(i)+"\t\t"+totalSalary.get(i));
 		}
+		return new EmpWage(salary,totalSalary,totalDays);
+	}
+	public static void disp() {
+		System.out.println("Day\tDaily Wage\tTotal Salary");
+		for(int i=1;i<totalDays;i++) {
+			System.out.println(i+"\t"+salary.get(i)+"\t\t"+totalSalary.get(i));
+			}
+
+	}
+	public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage Computation");
+	EmpWage.salary();
+	EmpWage.disp();
 	}
 }
