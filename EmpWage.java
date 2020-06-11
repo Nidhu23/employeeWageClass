3b9be25c87c311711fac3728e4712119e57f9460
“Nidhu23” <nidhunanik@gmail.com>
2020-06-11 19:34:02 +0530

    Added: Part Time Employee

--- EmpWage.java
+++ EmpWage.java
@@ -1,14 +1,21 @@
 import java.util.Random;
 public class EmpWage {
 	static final int ratePerHour=20;
+	static final int fullTime=1;
+	static final int partTime=2;
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation");
 	Random rand=new Random();
 	int empHr=0;
-	int empCheck=rand.nextInt(2);
-	if(empCheck==1) {
+	int empCheck=rand.nextInt(3);
+	if(empCheck==fullTime) {
 		empHr=8;
 		}
-	System.out.println("Daily Wage= "+(empHr*EmpWage.ratePerHour));
+	else if(empCheck==partTime) {
+		empHr=4;
+	}
+	else
+		empHr=0;
+	System.out.println("Daily Wage= "+(empHr*ratePerHour));
 	}
 }
