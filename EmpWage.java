43047ed7f86752bbd4a73b2a5f67320bf17487b7
“Nidhu23” <nidhunanik@gmail.com>
2020-06-11 20:04:39 +0530

    Added: Monthly Wage Computation Code

--- EmpWage.java
+++ EmpWage.java
@@ -1,20 +1,24 @@
 import java.util.Random;
 public class EmpWage {
-	static final int ratePerHour=20;
-	static final int fullTime=1;
-	static final int partTime=2;
+	private static final int ratePerHour=20;
+	private static final int fullTime=1;
+	private static final int partTime=2;
+	private static final int workingDays=20; 
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation");
 	Random rand=new Random();
-	int empHr=0;
+	int empHr=0,totalHrs=0;
+	for(int i=0;i<workingDays;i++) {
 	int empCheck=rand.nextInt(3);
 	switch(empCheck) {
-	case 1: empHr=8;
+	case fullTime: empHr=8;
 			break;
-	case 2: empHr=4;
+	case partTime: empHr=4;
 			break;
 	default: empHr=0;
 	}
-	System.out.println("Daily Wage= "+(empHr*ratePerHour));
+	totalHrs+=empHr;
+	}
+	System.out.println("Monthly Wage= "+(totalHrs*ratePerHour));
 	}
 }
